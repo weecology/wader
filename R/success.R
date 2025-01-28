edcab524a35c6d01181b064106b2133e07203a4a
Glenda Yenni <glenda@weecology.org>
2025-01-28 14:59:00 -0700

    Add overall success calculation

---
+++ R/success.R
@@ -0,0 +1,40 @@
+#' @name overall_success
+#'
+#' @title Generate summaries of nest success from data
+#'
+#' @description Create a table of overall nest success by species and year
+#'
+#' @param minyear Earliest year to include
+#' @param maxyear Most recent year to include
+#'
+#'
+#' @return a data.frame
+#'
+#' @export
+
+overall_success <- function(path = get_default_data_path(),
+                            minyear = 1986, maxyear = as.integer(format(Sys.Date(), "%Y")),
+                            download_if_missing = TRUE)
+{
+  load_datafile("Nesting/nest_success_summary.csv", path = path) %>%
+    dplyr::group_by(year, species) %>%
+    dplyr::mutate(species = toupper(species)) %>%
+    dplyr::filter(year <= maxyear, year >= minyear) %>%
+    dplyr::summarise(incubation_N=sum(incubation_k), incubation_sumy=sum(incubation_sumy),
+                     incubation_e=sum(incubation_e), incubation_j=mean(incubation_j),
+                     nestling_N=sum(nestling_k), nestling_sumy=sum(nestling_sumy), nestling_e=sum(nestling_e),
+                     nestling_j=mean(nestling_j)) %>%
+    dplyr::mutate(incubation_p = 1-((incubation_N-incubation_sumy)/incubation_e),
+                  incubation_Success = incubation_p^incubation_j,
+                  incubation_varp=(incubation_p*(1-incubation_p))/incubation_e,
+                  incubation_var = incubation_varp*((incubation_j*(incubation_p^(incubation_j-1)))^2),
+                  incubation_SD = sqrt(incubation_var),
+                  nestling_p = 1-((nestling_N-nestling_sumy)/nestling_e),
+                  nestling_Success = nestling_p^nestling_j,
+                  nestling_varp=(nestling_p*(1-nestling_p))/nestling_e,
+                  nestling_var = nestling_varp*((nestling_j*(nestling_p^(nestling_j-1)))^2),
+                  nestling_SD = sqrt(nestling_var),
+                  overall_Success = (incubation_p^incubation_j)*(nestling_p^nestling_j),
+                  overall_var = ((incubation_Success^2)*nestling_var)+((nestling_Success^2)*incubation_var)+(incubation_var*nestling_var),
+                  overall_SD = sqrt(overall_var))
+}
