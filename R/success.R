ab3d0318645314bf11fb00baba4798edbe0155a2
Glenda Yenni <glenda@weecology.org>
2025-09-28 14:38:53 -0600

    Ignore NAs when calculating overall success

--- R/success.R
+++ R/success.R
@@ -20,10 +20,14 @@ overall_success <- function(path = get_default_data_path(),
     dplyr::group_by(year, species) %>%
     dplyr::mutate(species = toupper(species)) %>%
     dplyr::filter(year <= maxyear, year >= minyear) %>%
-    dplyr::summarise(incubation_N=sum(incubation_k), incubation_sumy=sum(incubation_sumy),
-                     incubation_e=sum(incubation_e), incubation_j=mean(incubation_j),
-                     nestling_N=sum(nestling_k), nestling_sumy=sum(nestling_sumy), nestling_e=sum(nestling_e),
-                     nestling_j=mean(nestling_j)) %>%
+    dplyr::summarise(incubation_N=sum(incubation_k, na.rm=TRUE),
+                     incubation_sumy=sum(incubation_sumy, na.rm=TRUE),
+                     incubation_e=sum(incubation_e, na.rm=TRUE),
+                     incubation_j=mean(incubation_j, na.rm=TRUE),
+                     nestling_N=sum(nestling_k, na.rm=TRUE),
+                     nestling_sumy=sum(nestling_sumy, na.rm=TRUE),
+                     nestling_e=sum(nestling_e, na.rm=TRUE),
+                     nestling_j=mean(nestling_j, na.rm=TRUE)) %>%
     dplyr::mutate(incubation_p = 1-((incubation_N-incubation_sumy)/incubation_e),
                   incubation_Success = incubation_p^incubation_j,
                   incubation_varp=(incubation_p*(1-incubation_p))/incubation_e,
@@ -36,5 +40,6 @@ overall_success <- function(path = get_default_data_path(),
                   nestling_SD = sqrt(nestling_var),
                   overall_Success = (incubation_p^incubation_j)*(nestling_p^nestling_j),
                   overall_var = ((incubation_Success^2)*nestling_var)+((nestling_Success^2)*incubation_var)+(incubation_var*nestling_var),
-                  overall_SD = sqrt(overall_var))
+                  overall_SD = sqrt(overall_var)) %>%
+    dplyr::mutate_at(dplyr::vars(-dplyr::group_cols()), ~ifelse(is.nan(.), NA, .))
 }
