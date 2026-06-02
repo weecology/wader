bf5fde674671f1e933647d6723a6aafb2c74b85e
Glenda Yenni <glenda@weecology.org>
2026-06-02 14:41:21 -0600

    Add raw datapoints to indicator analyses graphs

--- R/indicator_analysis.R
+++ R/indicator_analysis.R
@@ -20,36 +20,25 @@ foraging_analysis <- function(path = get_default_data_path(),
                                 window = 3,
                                 download_if_missing = TRUE)
 {
-requireNamespace(ggplot2)
-#  requireNamespace(ggside)
+  library(ggplot2)
   foraging <- foraging_indicator(minyear = 2004) %>%
-    dplyr::mutate(period="baseline") %>%
-    dplyr::mutate(period = replace(period, year > 2016, "current"))
+    dplyr::mutate(period="baseline",
+                  period = replace(period, year > 2016, "current"),
+                  ymin=pmax(proportion_mean-proportion_sd,0),
+                  ymax=proportion_mean+proportion_sd)
 
-  ggplot(foraging, aes(x=year, y=proportion_mean,
-                       ymin=proportion_mean-proportion_sd, ymax=proportion_mean+proportion_sd)) +
-    geom_point(aes(color=period), size=2.2) +
+  ggplot(foraging, aes(x=year, y=proportion_mean, ymin=ymin, ymax=ymax)) +
+    geom_point(size=2.2) +
     geom_line(color="black", linewidth=1.1) +
-    geom_ribbon(alpha=0.2, fill="grey") +
-    geom_ribbon(alpha=0.5, aes(color=NULL, fill=period)) +
+    geom_ribbon(alpha=0.4) +
     geom_hline(yintercept=32, linetype=2, color="black", linewidth=1) +
+    geom_point(aes(x=year, y=proportion),shape = 8, size=2.2) +
     ylim(0,35) +
     labs(x="Year", y = "tactile/visual") +
     theme_classic() +
     theme(legend.position="none") +
-    guides(fill = guide_legend(byrow = TRUE)) +
-    # geom_ysidedensity(aes(fill=period), alpha = .5) +
-    # geom_ysidehline(yintercept = 32, linetype=2, color="darkgreen", linewidth=1) +
-    scale_color_grey() +
-    scale_fill_grey() +
     theme(
       text = element_text(family = "AppleGothic", size = 18),
-      # ggside.panel.border = element_blank(),
-      # ggside.panel.grid = element_blank(),
-      # ggside.panel.background = element_blank(),
-      # ggside.axis.text = element_blank(),
-      # ggside.axis.ticks = element_blank(),
-      # ggside.panel.scale = .2,
       axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0))) +
     guides(color = "none", fill = "none")
 }
@@ -77,35 +66,25 @@ initiation_analysis <- function(path = get_default_data_path(),
                                  download_if_missing = TRUE)
 {
 library(ggplot2)
-# library(ggside)
 initiation <- initiation_indicator(minyear = 2004) %>%
-              dplyr::mutate(period="baseline") %>%
-              dplyr::mutate(period = replace(period, year > 2016, "current"))
+              dplyr::mutate(period="baseline",
+                            period = replace(period, year > 2016, "current"),
+                            ymin=pmax(date_score_mean-date_score_sd,0),
+                            ymax=date_score_mean+date_score_sd)
 
-  ggplot(initiation, aes(x=year, y=date_score_mean,
-                       ymin=date_score_mean-date_score_sd, ymax=date_score_mean+date_score_sd)) +
-  geom_point(aes(color=period), size=2.2) +
+  ggplot(initiation, aes(x=year, y=date_score_mean, ymin=ymin, ymax=ymax)) +
+  geom_point(size=2.2) +
   geom_line(color="black", linewidth=1.1) +
-  geom_ribbon(alpha=0.2, fill="grey") +
-  geom_ribbon(alpha=0.5, aes(color=NULL, fill=period)) +
+  geom_ribbon(alpha=0.4) +
   geom_hline(yintercept=4.5, linetype=2, color="black", linewidth=1) +
+  geom_point(aes(x=year, y=date_score),shape = 8, size=2.2) +
   scale_y_reverse(limits=c(5,-.2), breaks=c(4,3,2,1,0),labels=c("December","January","February","March","April")) +
   labs(x="Year", y = "Date Score") +
   theme_classic() +
   theme(legend.position="none") +
   guides(fill = guide_legend(byrow = TRUE)) +
-  # geom_ysidedensity(aes(fill=period), alpha = .5) +
-  # geom_ysidehline(yintercept = 4.5, linetype=2, color="darkgreen", linewidth=1) +
-  scale_color_grey() +
-  scale_fill_grey() +
   theme(
       text = element_text(family = "AppleGothic", size = 18),
-      # ggside.panel.border = element_blank(),
-      # ggside.panel.grid = element_blank(),
-      # ggside.panel.background = element_blank(),
-      # ggside.axis.text = element_blank(),
-      # ggside.axis.ticks = element_blank(),
-      # ggside.panel.scale = .2,
       axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0))) +
     guides(color = "none", fill = "none")
 }
@@ -133,27 +112,23 @@ coastal_analysis <- function(path = get_default_data_path(),
                                 download_if_missing = TRUE)
 {
 library(ggplot2)
-# library(ggside)
   coastal <- coastal_indicator(minyear = 2004) %>%
-    dplyr::mutate(period="historic") %>%
-    dplyr::mutate(period = replace(period, year > 2016, "modern"))
+    dplyr::mutate(period="historic",
+                  period = replace(period, year > 2016, "modern"),
+                  ymin=pmax(proportion_mean-proportion_sd,0),
+                  ymax=proportion_mean+proportion_sd)
 
-  ggplot(coastal, aes(x=year, y=proportion_mean,
-                      ymin=proportion_mean-proportion_sd, ymax=proportion_mean+proportion_sd)) +
-    geom_point(aes(color=period), size=2.2) +
+  ggplot(coastal, aes(x=year, y=proportion_mean, ymin=ymin, ymax=ymax)) +
+    geom_point(size=2.2) +
     geom_line(color="black", linewidth=1.1) +
-    geom_ribbon(alpha=0.2, fill="grey") +
-    geom_ribbon(alpha=0.5, aes(color=NULL, fill=period)) +
+    geom_ribbon(alpha=0.4) +
     geom_hline(yintercept=.5, linetype=2, color="black", linewidth=1) +
+    geom_point(aes(x=year, y=proportion),shape = 8, size=2.2) +
     ylim(0,.6) +
     labs(x="Year", y = "proportion coastal") +
     theme_classic() +
     theme(legend.position="none") +
     guides(fill = guide_legend(byrow = TRUE)) +
-    # geom_ysidedensity(aes(fill=period), alpha = .5) +
-    # geom_ysidehline(yintercept = .5, linetype=2, color="darkgreen", linewidth=1) +
-    scale_color_grey() +
-    scale_fill_grey() +
     theme(
       text = element_text(family = "AppleGothic", size = 18),
       # ggside.panel.border = element_blank(),
@@ -189,35 +164,25 @@ supercolony_analysis <- function(path = get_default_data_path(),
                              download_if_missing = TRUE)
 {
 library(ggplot2)
-# library(ggside)
   supercolony <- supercolony_indicator(minyear = 2004) %>%
-    dplyr::mutate(period="historic") %>%
-    dplyr::mutate(period = replace(period, year > 2016, "modern"))
+    dplyr::mutate(period="historic",
+                  period = replace(period, year > 2016, "modern"),
+                  ymin=pmax(interval_mean-interval_sd,0),
+                  ymax=interval_mean+interval_sd)
 
-  ggplot(supercolony, aes(x=year, y=interval_mean,
-                          ymin=interval_mean-interval_sd, ymax=interval_mean+interval_sd)) +
-    geom_point(aes(color=period), size=2.2) +
+  ggplot(supercolony, aes(x=year, y=interval_mean, ymin=ymin, ymax=ymax)) +
+    geom_point(size=2.2) +
     geom_line(color="black", linewidth=1.1) +
-    geom_ribbon(alpha=0.2, fill="grey") +
-    geom_ribbon(alpha=0.5, aes(color=NULL, fill=period)) +
+    geom_ribbon(alpha=0.4) +
     geom_hline(yintercept=1.6, linetype=2, color="black", linewidth=1) +
+    geom_point(aes(x=year, y=ibis_interval),shape = 8, size=2.2) +
     ylim(0,3) +
     labs(x="Year", y = "mean interval") +
     theme_classic() +
     theme(legend.position="none") +
     guides(fill = guide_legend(byrow = TRUE)) +
-    # geom_ysidedensity(aes(fill=period), alpha = .5) +
-    # geom_ysidehline(yintercept = 1.6, linetype=2, color="darkgreen", linewidth=1) +
-    scale_color_grey() +
-    scale_fill_grey() +
     theme(
       text = element_text(family = "AppleGothic", size = 18),
-      # ggside.panel.border = element_blank(),
-      # ggside.panel.grid = element_blank(),
-      # ggside.panel.background = element_blank(),
-      # ggside.axis.text = element_blank(),
-      # ggside.axis.ticks = element_blank(),
-      # ggside.panel.scale = .2,
       axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0))) +
     guides(color = "none", fill = "none")
 }
